b7d15509bb18c652b2a44aaa8c4447fb372ee083
David Pires <78885070+David-Pires@users.noreply.github.com>
2021-10-24 12:59:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// function chkform()
+// {
+//     var form = document.getElementsByTagName('name')[0];
+//     var inputs = form.getElementsByTagName('input');
+
+//     for(var i = 0; i < inputs.length; i++){
+//         if(inputs[i].value === 'please fill your name')
+//             // do your error
+//     }
+// }
